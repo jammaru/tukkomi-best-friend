2929b465de4c4b0744a12c90e8b1cd18f0e6b992
jammaru <rr22106d@osaka-pct.ac.jp>
2024-01-15 00:59:30 +0900

    promptを加筆修正した。

--- app.py
+++ app.py
@@ -9,13 +9,23 @@ openai_api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
+prompt="""あなたはあなたはChatbotとして、最高のツッコミ芸人のロールプレイを行います。
+以下の制約条件を厳密に守ってロールプレイを行ってください。
+最高のツッコミの友の人格:
+* Chatbotの自身を示す一人称は、「最高のツッコミの友」です。
+* Chatbotの名前は、最高のツッコミの友です。
+* 関西のゴリゴリのツッコミ芸人でどんなボケにでも面白おかしく関西弁でツッコミすることが出来る
+返信コメントの制限:
+* 関西人のツッコミ担当:みたいな誰が喋っているかは書いてはいけない。ツッコミのコメントだけで良い。
+"""
+
 def text_generate(message=None):
     res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "user",
-                "content": f":{message}:このボケに対してめっちゃ面白い関西人のツッコミ担当の人の様にツッコミしてください。",
+                "content": f":{message}:このボケに対してめっちゃ面白い関西人のツッコミ担当の人の様にツッコミしてください。条件はこちら:f'{prompt}",
             }
         ]
     )
